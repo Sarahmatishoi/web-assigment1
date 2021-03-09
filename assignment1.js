0f68f23555a195a11758352bfa4bc640b71afeb8
sarahmatishoi <sarahmatishoi@gmail.com>
2021-03-09 17:39:11 +0300

    first commit

---
+++ assignment1.js
@@ -0,0 +1,9 @@
+function fruits(){
+    let selectedFruits=["Avocado","Grapes", "Mangoes", "Orange", "kiwi"];
+    for (let favourite = 0; favourite < selectedFruits.length; favourite++) {
+        const element = selectedFruits[favourite];
+        console.log(selectedFruits[favourite]);
+        
+    }
+}
+fruits();
